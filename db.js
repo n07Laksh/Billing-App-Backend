07c7a13c7196904e2409861024ff4f3fad462078
n07Laksh <dlaksh74@gmail.com>
2023-09-10 01:22:18 +0530

    mongoclient

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const uri = process.env.MONGODB_URL || "mongodb://0.0.0.0:27017/admin";
+const uri = "mongodb+srv://dlaksh74:M6yyDYl9BqGkJDII@cluster0.vpvkunm.mongodb.net/?retryWrites=true&w=majority" || "mongodb://0.0.0.0:27017/admin";
 
 const dbConnection = () => {
+
     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Connected to the database");
@@ -11,6 +12,14 @@ const dbConnection = () => {
         .catch((error) => {
             console.error("Error connecting to the database:", error);
         });
+
+
+    // Check for successful connection
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    db.once('open', () => {
+        console.log('Connected to MongoDB Atlas');
+    });
 };
 
 module.exports = dbConnection;
