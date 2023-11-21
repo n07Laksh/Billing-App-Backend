be9a8d9a798a3d8187c1dda43dc52bc6b8717537
n07Laksh <dlaksh74@gmail.com>
2023-11-21 16:16:06 +0530

    ..

--- app.js
+++ app.js
@@ -28,5 +28,5 @@ app.get("/home",(req,res)=>{
     return res.json({hell:"hello ji how are your"})
 })
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 app.listen(port, () => console.log("Listening on port " + port));
